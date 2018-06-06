6a14c643050ac8697bd382eda9383bbc43d7bb20
Gary Siu <Gary@untypeable.com>
2018-06-06 06:22:21 +0100

    added db seed and basic html for first render

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :items
-  root to: 'items#index'
+  root to: redirect('/items')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
