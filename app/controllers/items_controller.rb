19862e13bcfdd3a0d26bb20daf0cf85b429c8821
Gary Siu <Gary@untypeable.com>
2018-06-06 07:31:57 +0100

    added method to clear list and basic error message

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,11 @@ class ItemsController < ApplicationController
   end
   def create
     @item = Item.new(params.permit(:name))
-    @item.save
+    unless @item.save then flash[:alert] = "There was a problem saving the item (did you leave it blank?)" end
+    redirect_to items_url
+  end
+  def destroy
+    Item.delete_all
     redirect_to items_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items
-  root to: redirect('/items')
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items, :only => [:index, :create]
+  delete :items, to: "items#destroy"
+  root to: redirect("/items")
 end
