fb94d7006cb98a96c32bf1d165a1ca7fdc28049f
Gary Siu <Gary@untypeable.com>
2018-06-06 07:03:48 +0100

    MVP. Added validation, form handling and debugging

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  def create
+    @item = Item.new(params.permit(:name))
+    @item.save
+    redirect_to items_url
+  end
 end
